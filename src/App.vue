d4938559c8c734093178cafd78c56c5ea030b128
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2019-06-08 19:15:31 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <router-view id="content" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components: { Navbar
   }
 }
 </script>
 
 <style>
+
+body {
+  margin: 0px;
+  padding: 0px;
+}   
+
+#content {
+  width: 70%;
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <nav>
+      <router-link id='link' to='/' exact>Home</router-link>
+      <router-link id='link' to='/about' exact>About</router-link>
+      <a id='link' target="_blank" href="https://github.com/eltsu7/">GitHub</a>
+      <a id='link' target="_blank" href="https://eeli.kuvat.fi/">Gallery</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+}
+</script>
+
+<style scoped>
+nav {
+  margin: 0px;
+  padding: 0px;
+  overflow: hidden;
+  list-style-type: none;
+  display: block;
+  background-color: #333;
+}
+
+#link {
+  float: left;
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 16px 20px;
+  text-decoration: none;
+}
+
+#link.router-link-active {
+  color: #bbb;
+  background-color: #444;
+}
+
+#link:hover {
+  background-color: #555;
+}
+
+
+</style>
